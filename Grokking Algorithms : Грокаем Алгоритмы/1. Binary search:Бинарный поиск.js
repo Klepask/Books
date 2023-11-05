b554461b892248fd66d64d7c1644783bc3d2efa5
Eva Ulnitskaya <evaulnitsakaya@yandex.ru>
2023-11-05 22:46:25 +0300

    update Grokking Algorithms

--- Grokking Algorithms : Грокаем Алгоритмы/1. Binary search:Бинарный поиск.js
+++ Grokking Algorithms : Грокаем Алгоритмы/1. Binary search:Бинарный поиск.js
@@ -1,1 +1,21 @@
- Binary search/Бинарный поиск.js
+const binarySearch = (list, item) => {
+
+  if (list.length === 0) return -1;
+
+    low = 0;
+    hight = list[list.length -1];
+
+  while (low <= hight) {
+
+    let mid = Math.floor((low + hight) / 2);
+    
+    if (list[mid] === item) {
+      return mid;
+    }else if (list[mid] < item) {
+      low = mid + 1;
+    }else{
+      hight = mid - 1;
+    }
+  }
+  return -1; 
+}
