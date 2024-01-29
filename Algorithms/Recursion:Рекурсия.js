48489246721bd2b5e8b6c47a0a199449b6d7f7c1
Eva Ulnitskaya <evaulnitsakaya@yandex.ru>
2024-01-29 22:51:48 +0300

    add recursion example

--- Algorithms/Recursion:Рекурсия.js
+++ Algorithms/Recursion:Рекурсия.js
@@ -1,1 +1,15 @@
+function findKey(boxes) {
+  if (!boxes.length) {
+    return null;
+  }
+
+  const box = boxes.pop();
+  if (Array.isArray(box)) {
+    boxes.push(...box);
+  } else if (box === 'key') {
+    return box;
+  }
+
+  return findKey(boxes);
+}
 
